e92258117bba45682c045a1d3a2d6e5cb6082c61
AlexanderLeshko <leshkoan@ya.ru>
2024-04-07 20:27:51 +0400

    Funish

--- Spacebattle/Spacebattle/PluginLoader/PluginLoader.cs
+++ Spacebattle/Spacebattle/PluginLoader/PluginLoader.cs
@@ -1,14 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Spacebattle.PluginLoader
 {
-    internal class PluginLoader
+    interface IPlugin
     {
-        // Путь к каталогу с плагинами
-        private const string pathPlugins = @"d:\plugins";
+        void Load();
+    }
+
+    class PluginLoader
+    {
+        private readonly Queue<IPlugin> _pluginQueue = new Queue<IPlugin>();
+        private readonly object _lock = new object();
+
+        public void ScanPlugins(string folderPath)
+        {
+            string[] pluginFiles = Directory.GetFiles(folderPath, "*.dll");
+
+            foreach (var pluginFile in pluginFiles)
+            {
+                Assembly assembly = Assembly.LoadFrom(pluginFile);
+
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (typeof(IPlugin).IsAssignableFrom(type))
+                    {
+                        IPlugin? plugin = Activator.CreateInstance(type) as IPlugin;
+                        if (plugin != null) EnqueuePlugin(plugin);
+                    }
+                }
+            }
+        }
+
+        private void EnqueuePlugin(IPlugin plugin)
+        {
+            lock (_lock)
+            {
+                _pluginQueue.Enqueue(plugin);
+            }
+        }
+
+        public void LoadPlugins()
+        {
+            while (true)
+            {
+                IPlugin plugin = DequeuePlugin();
+
+                try
+                {
+                    plugin.Load();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error loading plugin: {ex.Message}");
+                    EnqueuePlugin(plugin); // Повторная попытка загрузки плагина
+                }
+            }
+        }
+
+        private IPlugin DequeuePlugin()
+        {
+            lock (_lock)
+            {
+                return _pluginQueue.Dequeue();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PluginLoader pluginLoader = new PluginLoader();
+            string pluginsFolderPath = "C:\\plugins";
+
+            Thread scanThread = new Thread(() => pluginLoader.ScanPlugins(pluginsFolderPath));
+            Thread loadThread = new Thread(pluginLoader.LoadPlugins);
+
+            scanThread.Start();
+            loadThread.Start();
+
+            scanThread.Join();
+            loadThread.Join();
+        }
     }
 }
