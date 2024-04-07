722e7b104dcdf1354b78e7131075ab863dd6a7a6
AlexanderLeshko <leshkoan@ya.ru>
2024-04-07 20:13:16 +0400

    Выполнение домашнего задания №4

---
+++ Spacebattle/Spacebattle/PluginLoader/PluginLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spacebattle.PluginLoader
+{
+    internal class PluginLoader
+    {
+        // Путь к каталогу с плагинами
+        private const string pathPlugins = @"d:\plugins";
+    }
+}
