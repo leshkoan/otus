94e53069579055b32efc2ccc57945244e7f94e41
AlexanderLeshko <leshkoan@ya.ru>
2024-04-25 15:34:03 +0400

    HW#6-Alexander_Leshko

---
+++ Spacebattle/Spacebattle/Commands.cs
@@ -0,0 +1,60 @@
+﻿using SpaceBattle.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+/// <summary>
+// Команда 1
+/// </summary>
+class Command1 : ICommand
+{
+    ICommand _next;
+    public Command1(ICommand next) => _next = next;
+    public async Task ExecuteAsync()
+    {
+        await _next.ExecuteAsync();
+        Console.WriteLine("Команда1");
+    }
+}
+
+/// <summary>
+// Команда 2
+/// </summary>
+class Command2 : ICommand
+{
+    ICommand _next;
+    public Command2(ICommand next) => _next = next;
+
+    public async Task ExecuteAsync()
+    {
+        Console.WriteLine("Команда2");
+        if (_next != null)
+            await _next.ExecuteAsync();
+    }
+}
+
+/// <summary>
+// Команда 3
+/// </summary>
+class Command3 : ICommand
+{
+    ICommand _next;
+    public Command3(ICommand next) => _next = next;
+
+    public async Task ExecuteAsync()
+    {
+        Console.WriteLine("Команда3");
+        if (_next != null)
+            await _next.ExecuteAsync();
+    }
+}
+
+/// <summary>
+// Команда для завершения работы
+/// </summary>
+class finishCommand : ICommand
+{
+    public async Task ExecuteAsync()
+    {
+        await Task.Delay(200);
+    }
+}
---
+++ Spacebattle/Spacebattle/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        var finishCommand = new finishCommand();
+        var command3 = new Command3(finishCommand);
+        var command2 = new Command2(command3);
+        var command1 = new Command1(command2);
+
+        await command1.ExecuteAsync();
+    }
+}
